5654f087bb6f1e06919f8ff840ed3dbcb193c407
baontq23 <baooshacker@gmail.com>
2023-03-24 02:45:14 +0700

    feat: add route for product model

---
+++ src/routes/v1/product.route.ts
@@ -0,0 +1,19 @@
+import express, { Router } from 'express';
+import { validate } from '../../modules/validate';
+import { auth } from '../../modules/auth';
+import { productController, productValidation } from '../../modules/product';
+
+const router: Router = express.Router();
+
+router
+  .route('/')
+  .post(auth('manageProducts'), validate(productValidation.createProduct), productController.createProduct)
+  .get(auth('getProducts'), validate(productValidation.getProductsByUser), productController.getProductsByUser);
+
+router
+  .route('/:productId')
+  .get(auth('getProducts'), validate(productValidation.getProduct), productController.getProduct)
+  .patch(auth('manageProducts'), validate(productValidation.updateProduct), productController.updateProduct)
+  .delete(auth('manageProducts'), validate(productValidation.deleteProduct), productController.deleteProduct);
+
+export default router;
